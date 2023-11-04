e53502c8c12fd75c298cd98cec2e72045782af12
amakura-411 <callplus411@gmail.com>
2023-11-04 15:51:29 +0900

    Revert "add:server.go"
    This reverts commit a120eddd28dff5477b811faceb8a149f7cebfc11.

--- backend/main.go
+++ backend/main.go
@@ -1,13 +1,6 @@
 package main
-
-import (
-	"fmt"
-	"go-next-app/backend/server"
-)
-
+import "fmt"
 // hello World!!をコンソールに表示する
 func main() {
 	fmt.Println("Hello World!!")
-	server.StartServer()
-
 }
