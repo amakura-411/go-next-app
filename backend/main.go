a120eddd28dff5477b811faceb8a149f7cebfc11
amakura-411 <callplus411@gmail.com>
2023-11-04 15:42:16 +0900

    add:server.go

--- backend/main.go
+++ backend/main.go
@@ -1,6 +1,13 @@
 package main
-import "fmt"
+
+import (
+	"fmt"
+	"go-next-app/backend/server"
+)
+
 // hello World!!をコンソールに表示する
 func main() {
 	fmt.Println("Hello World!!")
+	server.StartServer()
+
 }
